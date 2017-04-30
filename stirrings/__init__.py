6fd1a7c874a96149a3b29658c0f2e1ac8a65a3c1
Trevor Bača <trevor.baca@gmail.com>
2017-04-29 19:52:09 -0500

    Cleaned up package initializer.

--- stirrings/__init__.py
+++ stirrings/__init__.py
@@ -1,4 +1,13 @@
 # -*- coding: utf-8 -*-
+import distutils.version
+import platform
+if not (
+    distutils.version.LooseVersion('3.5') <
+    distutils.version.LooseVersion(platform.python_version())
+    ):
+    raise ImportError('Requires Python 3.5.')
+del distutils
+del platform
 from stirrings import tools
 from stirrings import materials
 from stirrings import segments
