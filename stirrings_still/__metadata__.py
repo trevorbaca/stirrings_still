e757777927997b6e88f441480be870550ab07267
Trevor Bača <trevor.baca@gmail.com>
2017-05-04 16:38:15 -0500

    Updated typed ordered dict references.

--- stirrings_still/__metadata__.py
+++ stirrings_still/__metadata__.py
@@ -2,7 +2,7 @@
 import abjad
 
 
-metadata = abjad.datastructuretools.TypedOrderedDict(
+metadata = abjad.TypedOrderedDict(
     [
         ('catalog_number', 'AWN-018'),
         ('composer', 'Bača'),
