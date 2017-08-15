d991d66a7763f21a4b7082c070eff93309ed99a5
Trevor Bača <trevor.baca@gmail.com>
2017-08-15 11:05:25 -0500

    Cleaned up source.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     setuptools.setup(
         author='Trevor Bača',
         author_email='trevor.baca@gmail.com',
-        install_requires = (
+        install_requires=(
             'abjad',
             ),
         name='stirrings_still',
