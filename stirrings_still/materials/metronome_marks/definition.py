aa085184c697d9e488269295e3ee073bcd521557
Trevor Bača <trevor.baca@gmail.com>
2018-04-02 10:10:25 -0500

    Metronome marks.

---
+++ stirrings_still/materials/metronome_marks/definition.py
@@ -0,0 +1,29 @@
+import abjad
+
+
+metronome_marks = abjad.OrderedDict([
+    (
+        'larghissimo',
+        abjad.MetronomeMark((1, 4), 39),
+        ),
+    (
+        'largo',
+        abjad.MetronomeMark((1, 4), 52),
+        ),
+    (
+        'adagio',
+        abjad.MetronomeMark((1, 4), 65),
+        ),
+    (
+        'andante',
+        abjad.MetronomeMark((1, 4), 91),
+        ),
+    (
+        'allegro',
+        abjad.MetronomeMark((1, 4), 117),
+        ),
+    (
+        'presto',
+        abjad.MetronomeMark((1, 4), 169),
+        ),
+    ])
