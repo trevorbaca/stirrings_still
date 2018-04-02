634ac686dab87889261a3d54725b79afb99d83d8
Trevor Bača <trevor.baca@gmail.com>
2018-04-02 10:10:25 -0500

    Metronome marks.

--- stirrings_still/materials/__init__.py
+++ stirrings_still/materials/__init__.py
@@ -1,3 +1,4 @@
 from .instruments.definition import instruments
 from .margin_markups.definition import margin_markups
+from .metronome_marks.definition import metronome_marks
 from .time_signature_series.definition import time_signature_series
