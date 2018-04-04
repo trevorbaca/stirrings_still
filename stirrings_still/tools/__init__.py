3a78e7986ff84faafe064504e19a4d072dbca746
Trevor Bača <trevor.baca@gmail.com>
2018-04-04 10:50:09 -0500

    Time.

--- stirrings_still/tools/__init__.py
+++ stirrings_still/tools/__init__.py
@@ -1,5 +1,6 @@
+from .FirstOrderStageSpecifier import FirstOrderStageSpecifier
 from .ScoreTemplate import ScoreTemplate
+from .first_order_time_signatures import first_order_time_signatures
 from .margin_markup import margin_markup
 from .relata import relata
 from .time_signatures import time_signatures
-from .weave import weave
