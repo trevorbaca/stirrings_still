1f63f7a67370ad8d59cc29090974908b8f6e3f3c
Trevor Bača <trevor.baca@gmail.com>
2018-04-04 11:26:58 -0500

    Time.

---
+++ stirrings_still/tools/StageToken.py
@@ -0,0 +1,74 @@
+import abjad
+
+
+class StageToken(abjad.AbjadObject):
+    r'''Stage token.
+    '''
+
+    ### CLASS VARIABLES ###
+
+    __slots__ = (
+        '_description',
+        '_length',
+        '_letter',
+        '_number',
+        )
+
+    _descriptions = (
+        'clearing',
+        'conclusion',
+        'development',
+        'field',
+        'inception',
+        'isolated state',
+        'pause',
+        'repetatum',
+        )
+
+    ### INITIALIZER ###
+
+    def __init__(
+        self,
+        letter=None,
+        number=None,
+        description=None,
+        length=None,
+        ):
+        if description is not None:
+            assert description in self._descriptions, repr(description)
+        self._description = description
+        if length is not None:
+            assert isinstance(length, int)
+        self._length = length
+        if letter is not None:
+            assert isinstance(letter, str)
+        self._letter = letter
+        if number is not None:
+            assert isinstance(number, int)
+        self._number = number
+
+    ### PUBLIC PROPERTIES ###
+
+    @property
+    def description(self):
+        r'''Gets description.
+        '''
+        return self._description
+
+    @property
+    def length(self):
+        r'''Gets length.
+        '''
+        return self._length
+
+    @property
+    def letter(self):
+        r'''Gets letter.
+        '''
+        return self._letter
+
+    @property
+    def number(self):
+        r'''Gets number.
+        '''
+        return self._number
