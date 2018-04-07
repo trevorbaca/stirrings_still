1099bcf865ce19f3e6ef66e4bc13d1b20910074b
Trevor Bača <trevor.baca@gmail.com>
2018-04-07 17:37:17 -0500

    S.

---
+++ stirrings_still/segments/S/__metadata__.py
@@ -0,0 +1,175 @@
+import abjad
+
+
+metadata = abjad.OrderedDict(
+    [
+        (
+            'alive_during_segment',
+            [
+                'Score',
+                'GlobalContext',
+                'GlobalRests',
+                'GlobalSkips',
+                'MusicContext',
+                'StringQuartetStaffGroup',
+                'ViolinIMusicStaff',
+                'ViolinIMusicVoice',
+                'ViolinIIMusicStaff',
+                'ViolinIIMusicVoice',
+                'ViolaMusicStaff',
+                'ViolaMusicVoice',
+                'CelloMusicStaff',
+                'CelloMusicVoice',
+                ],
+            ),
+        (
+            'bol_measure_numbers',
+            [812, 829],
+            ),
+        ('duration', "2'47''"),
+        (
+            'fermata_measure_numbers',
+            [846],
+            ),
+        ('first_measure_number', 812),
+        ('last_measure_is_fermata', True),
+        ('last_measure_number', 846),
+        (
+            'persistent_indicators',
+            abjad.OrderedDict(
+                [
+                    (
+                        'CelloMusicStaff',
+                        [
+                            abjad.Momento(
+                                context='CelloMusicVoice',
+                                prototype='abjad.Clef',
+                                value='bass',
+                                ),
+                            abjad.Momento(
+                                context='CelloMusicVoice',
+                                prototype='abjad.Instrument',
+                                value='Cello',
+                                ),
+                            ],
+                        ),
+                    (
+                        'Score',
+                        [
+                            abjad.Momento(
+                                context='GlobalSkips',
+                                prototype='abjad.MetronomeMark',
+                                value='largo',
+                                ),
+                            abjad.Momento(
+                                context='GlobalSkips',
+                                prototype='abjad.TimeSignature',
+                                value='1/4',
+                                ),
+                            ],
+                        ),
+                    (
+                        'ViolaMusicStaff',
+                        [
+                            abjad.Momento(
+                                context='ViolaMusicVoice',
+                                prototype='abjad.Clef',
+                                value='alto',
+                                ),
+                            abjad.Momento(
+                                context='ViolaMusicVoice',
+                                prototype='abjad.Instrument',
+                                value='Viola',
+                                ),
+                            ],
+                        ),
+                    (
+                        'ViolinIIMusicStaff',
+                        [
+                            abjad.Momento(
+                                context='ViolinIIMusicVoice',
+                                prototype='abjad.Clef',
+                                value='treble',
+                                ),
+                            abjad.Momento(
+                                context='ViolinIIMusicVoice',
+                                prototype='abjad.Instrument',
+                                value='ViolinI',
+                                ),
+                            ],
+                        ),
+                    (
+                        'ViolinIMusicStaff',
+                        [
+                            abjad.Momento(
+                                context='ViolinIMusicVoice',
+                                prototype='abjad.Clef',
+                                value='treble',
+                                ),
+                            abjad.Momento(
+                                context='ViolinIMusicVoice',
+                                prototype='abjad.Instrument',
+                                value='ViolinI',
+                                ),
+                            ],
+                        ),
+                    ]
+                ),
+            ),
+        ('segment_name', 'S'),
+        ('segment_number', 19),
+        (
+            'sounds_during_segment',
+            abjad.OrderedDict(
+                [
+                    ('CelloMusicVoice', False),
+                    ('ViolaMusicVoice', False),
+                    ('ViolinIIMusicVoice', False),
+                    ('ViolinIMusicVoice', False),
+                    ]
+                ),
+            ),
+        ('start_clock_time', "36'46''"),
+        ('stop_clock_time', "39'33''"),
+        (
+            'time_signatures',
+            [
+                '4/4',
+                '3/4',
+                '4/4',
+                '5/4',
+                '6/4',
+                '6/4',
+                '3/4',
+                '4/4',
+                '5/4',
+                '4/4',
+                '3/4',
+                '4/4',
+                '4/4',
+                '4/4',
+                '3/4',
+                '5/4',
+                '6/4',
+                '3/4',
+                '4/4',
+                '4/4',
+                '5/4',
+                '6/4',
+                '3/4',
+                '3/4',
+                '4/4',
+                '4/4',
+                '4/4',
+                '3/4',
+                '4/4',
+                '3/4',
+                '4/4',
+                '5/4',
+                '6/4',
+                '6/4',
+                '1/4',
+                ],
+            ),
+        ]
+    )
---
+++ stirrings_still/segments/S/definition.py
@@ -0,0 +1,26 @@
+import abjad
+import baca
+import stirrings_still
+
+
+###############################################################################
+##################################### [S] #####################################
+###############################################################################
+
+maker = baca.SegmentMaker(
+    metronome_mark_stem_height=1.5,
+    time_signatures=stirrings_still.time_signatures('S'),
+    validate_measure_count=35,
+    )
+
+maker(
+    'GlobalSkips',
+    baca.rehearsal_mark('S'),
+    baca.rehearsal_mark_y_offset(12),
+    )
+
+time = (
+    ('long', 35),
+    )
+
+stirrings_still.time(maker, time)
