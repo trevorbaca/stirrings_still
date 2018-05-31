e09026230d826bbf19c2c6b0e652de4198365bf1
Trevor Bača <trevor.baca@gmail.com>
2018-05-31 13:41:06 -0500

    Cleaned up Travis configfile.

--- setup.py
+++ setup.py
@@ -2,14 +2,29 @@
 import setuptools
 
 
+install_requires = [
+    'abjad',
+    'mypy',
+    'roman',
+    ]
+
+keywords = [
+    'abjad',
+    'music composition',
+    'music notation',
+    'formalized score control',
+    'lilypond',
+    ]
+keywords = ', '.join(keywords)
+
 if __name__ == '__main__':
     setuptools.setup(
         author='Trevor Bača',
         author_email='trevor.baca@gmail.com',
-        install_requires=('abjad',),
-        name='stirrings_still',
-        packages=('stirrings_still',),
+        install_requires=install_requires,
+        keywords=keywords,
+        name='Stirrings Still',
+        packages=['stirrings_still'],
+        paltforms='Any',
         url='https://github.com/trevorbaca/stirrings_still',
-        version='0.1',
-        zip_safe=False,
         )
