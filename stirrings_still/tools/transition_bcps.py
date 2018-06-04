f1f32d98029c2cdae30dd430587d57503b608c47
Trevor Bača <trevor.baca@gmail.com>
2018-06-04 16:22:32 -0500

    I.

---
+++ stirrings_still/tools/transition_bcps.py
@@ -0,0 +1,15 @@
+import baca
+
+
+def transition_bcps(*, rotation=0, tweaks=None):
+    """
+    Makes transition bow contact points.
+    """
+    bcps = baca.sequence([
+        (1, 7), (3, 7), 
+        (1, 7), (4, 7),
+        (1, 7), (5, 7),
+        (1, 7), (6, 7),
+        (1, 7), (7, 7),
+        ])
+    return baca.bcps(bcps, tweaks=tweaks)
