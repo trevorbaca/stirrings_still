8c4aca509d36119c1ef210a42c674745c8578dcf
Trevor Bača <trevor.baca@gmail.com>
2018-06-08 13:28:39 -0500

    D.

--- stirrings_still/tools/flight.py
+++ stirrings_still/tools/flight.py
@@ -3,19 +3,26 @@ import baca
 from abjadext import rmakers
 
 
-def flight(counts, *, rotation=0):
+def flight(counts, rotation):
     """
     Makes flight rhythm.
     """
-
     counts = baca.sequence(counts)
     extra_counts = baca.sequence([1, 0, 2]).rotate(n=rotation)
-
     return baca.rhythm(
         rhythm_maker=rmakers.TaleaRhythmMaker(
+            extra_counts_per_division=extra_counts,
             talea=rmakers.Talea(
                 counts=counts,
                 denominator=8,
+                ),
+            tuplet_specifier=rmakers.TupletSpecifier(
+                avoid_dots=True,
+                extract_trivial=True,
+                diminution=True,
+                denominator=(1, 16),
+                force_fraction=True,
+                trivialize=True,
                 ),
             ),
         )
