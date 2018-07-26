0231e47877063e36d29e0c49b69d89b023150121
Trevor Bača <trevor.baca@gmail.com>
2018-07-26 10:56:06 -0500

    S.

--- stirrings_still/segments/S/definition.py
+++ stirrings_still/segments/S/definition.py
@@ -36,14 +36,30 @@ maker(
     )
 
 maker(
-    (['v1', 'v2'], [5, (10, 11), (16, 18), (23, 26)]),
-    baca.hairpin('mp > pp'),
+    (['v1', 'v2', 'vc'], [5, (10, 11), (16, 18), (23, 26)]),
+    baca.match(
+        [0, 4],
+        baca.markup('extremely wide, fast circles'),
+        ),
     baca.match(
         [0, 1, 2, 3],
+        baca.hairpin(
+            'p > ppp <',
+            piece_selector=baca.mgroups(),
+            ),
         stirrings_still.wave((1, 4), (3, 32)),
         ),
     baca.match(
         [4, 5, 6, 7],
+        baca.hairpin(
+            'ppp < p >',
+            piece_selector=baca.mgroups(),
+            ),
         stirrings_still.wave((5, 32), (1, 4)),
+        ),
+    baca.match(
+        [8, 9, 10, 11],
+        baca.make_repeat_tied_notes(),
+        #baca.pitch('B0'),
         ),
     )
