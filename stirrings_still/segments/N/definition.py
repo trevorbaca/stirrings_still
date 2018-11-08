ef4bb85124c0fcc9d2efc39f5fc38e9f05e41d10
Trevor Bača <trevor.baca@gmail.com>
2018-11-07 19:15:53 -0600

    N.

--- stirrings_still/segments/N/definition.py
+++ stirrings_still/segments/N/definition.py
@@ -92,74 +92,73 @@ maker(
     )
 
 maker(
-    'v1',
+    ('v1', (13, 16)),
     baca.hairpin(
         'niente o< f',
         selector=baca.leaves().rleak(),
-        measures=(13, 16),
+        )
+    )
+
+maker(
+    ('v1', (13, 22)),
+    stirrings_still.clouded_pane(),
+    )
+
+maker(
+    ('v1', (23, 28)),
+    baca.accent(
+        selector=baca.pheads(),
         ),
-    baca.new(
-        stirrings_still.clouded_pane(),
-        measures=(13, 22),
+    baca.hairpin(
+        'f < ff >',
+        pieces=baca.plts()[:-1].partition_by_counts(
+            [2], cyclic=True),
+        ),
+    baca.scp_spanner(
+        'T -> P ->',
+        abjad.tweak(5.5).staff_padding,
+        autodetect_right_padding=False,
+        pieces=baca.plts()[:-1],
+        ),
+    baca.stem_tremolo(
+        selector=baca.pleaves(),
+        ),
+    stirrings_still.flight('C', 0, start=0),
+    )
+
+maker(
+    ('v1', 29),
+    baca.hairpin(
+        'mp -- !',
+        abjad.tweak(True).to_barline,
+        selector=baca.leaves().rleak(),
+        ),
+    baca.make_repeat_tied_notes(),
+    baca.stem_tremolo(
+        selector=baca.pleaves(),
         ),
     )
 
 maker(
-    'v1',
-    baca.new(
-        baca.accent(
-            selector=baca.pheads(),
-            ),
-        baca.hairpin(
-            'f < ff >',
-            pieces=baca.plts()[:-1].partition_by_counts(
-                [2], cyclic=True),
-            ),
-        baca.scp_spanner(
-            'T -> P ->',
-            abjad.tweak(5.5).staff_padding,
-            autodetect_right_padding=False,
-            pieces=baca.plts()[:-1],
-            ),
-        baca.stem_tremolo(
-            selector=baca.pleaves(),
-            ),
-        stirrings_still.flight('C', 0, start=0),
-        measures=(23, 28),
+    ('v1', (30, 35)),
+    baca.accent(
+        selector=baca.pheads(),
         ),
-    baca.new(
-        baca.hairpin(
-            'mp -- !',
-            abjad.tweak(True).to_barline,
-            selector=baca.leaves().rleak(),
-            ),
-        baca.make_repeat_tied_notes(),
-        baca.stem_tremolo(
-            selector=baca.pleaves(),
-            ),
-        measures=29,
+    baca.hairpin(
+        'ff < fff >',
+        pieces=baca.plts()[:-1].partition_by_counts(
+            [2], cyclic=True),
         ),
-    baca.new(
-        baca.accent(
-            selector=baca.pheads(),
-            ),
-        baca.hairpin(
-            'ff < fff >',
-            pieces=baca.plts()[:-1].partition_by_counts(
-                [2], cyclic=True),
-            ),
-        baca.scp_spanner(
-            'O -> P ->',
-            abjad.tweak(5.5).staff_padding,
-            autodetect_right_padding=False,
-            pieces=baca.plts()[:-1],
-            ),
-        baca.stem_tremolo(
-            selector=baca.pleaves(),
-            ),
-        stirrings_still.flight('C', 0, start=1),
-        measures=(30, 35),
+    baca.scp_spanner(
+        'O -> P ->',
+        abjad.tweak(5.5).staff_padding,
+        autodetect_right_padding=False,
+        pieces=baca.plts()[:-1],
         ),
+    baca.stem_tremolo(
+        selector=baca.pleaves(),
+        ),
+    stirrings_still.flight('C', 0, start=1),
     )
 
 maker(
