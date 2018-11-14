de2d1aa866eab6aadc301065b22242ed6b73ebab
Trevor Bača <trevor.baca@gmail.com>
2018-11-14 08:43:55 -0600

    E.

---
+++ stirrings_still/tools/multistage_leaf_glissando.py
@@ -0,0 +1,66 @@
+import abjad
+import baca
+
+
+def multistage_leaf_glissando(pairs, final_pitch, rleak_final_stage=None):
+    """
+    Makes multistage leaf glissando.
+    """
+    assert isinstance(pairs, list), repr(pairs)
+    assert all(isinstance(_, tuple) for _ in pairs), repr(pairs)
+
+    commands = []
+    command = baca.untie_to(
+        selector=baca.leaves(),
+        )
+    commands.append(command)
+
+    start = 0
+    for pair_1, pair_2 in baca.sequence(pairs).nwise():
+        start_pitch, leaf_count = pair_1
+        stop_pitch = pair_2[0]
+        assert isinstance(start_pitch, str), repr(start_pitch)
+        assert isinstance(stop_pitch, str), repr(stop_pitch)
+        assert isinstance(leaf_count, int), repr(leaf_count)
+        stop = start + leaf_count
+        command = baca.chunk(
+            baca.glissando(
+                allow_repeats=True,
+                stems=True,
+                selector=baca.leaves()[start:stop],
+                ),
+            baca.interpolate_staff_positions(
+                start_pitch,
+                stop_pitch,
+                selector=baca.leaves()[start:stop],
+                ),
+            )
+        commands.append(command)
+        start = stop - 1
+
+    pair = pairs[-1]
+    start_pitch, leaf_count = pair
+    assert isinstance(start_pitch, str), repr(start_pitch)
+    assert isinstance(leaf_count, (int, type(None))), repr(leaf_count)
+    if leaf_count is None:
+        stop = None
+    else:
+        stop = start + leaf_count
+    selector = baca.leaves()
+    if rleak_final_stage:
+        selector = selector.rleak()
+    selector = selector[start:stop]
+    command = baca.chunk(
+        baca.glissando(
+            allow_repeats=True,
+            stems=True,
+            selector=selector,
+            ),
+        baca.interpolate_staff_positions(
+            start_pitch,
+            final_pitch,
+            selector=selector,
+            ),
+        )
+    commands.append(command)
+    return baca.chunk(*commands)
