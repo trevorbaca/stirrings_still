2dedcadc62858d29c75fb4a80b2d221a85d40ea8
Trevor Bača <trevor.baca@gmail.com>
2019-02-26 10:20:34 -0600

    Cleaned up persist files.

--- stirrings_still/segments/E/__metadata__.py
+++ stirrings_still/segments/E/__metadata__.py
@@ -4,29 +4,6 @@ import abjad
 metadata = abjad.OrderedDict(
     [
         (
-            'alive_during_segment',
-            [
-                'Score',
-                'Global_Context',
-                'Global_Skips',
-                'Music_Context',
-                'String_Quartet_Staff_Group',
-                'Violin_I_Music_Staff',
-                'Global_Rests',
-                'Violin_I_Music_Voice',
-                'Violin_I_Rest_Voice',
-                'Violin_II_MusicStaff',
-                'Violin_II_Music_Voice',
-                'Violin_II_Rest_Voice',
-                'Viola_Music_Staff',
-                'Viola_Music_Voice',
-                'Viola_Rest_Voice',
-                'Cello_Music_Staff',
-                'Cello_Music_Voice',
-                'Cello_Rest_Voice',
-                ],
-            ),
-        (
             'bol_measure_numbers',
             [
                 306,
