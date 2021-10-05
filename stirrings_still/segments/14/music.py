7d71fdc25b5575933b8b0afae325b4867938205c
Trevor Bača <trevor.baca@gmail.com>
2021-10-05 12:18:40 +0200

    Bundled LilyPond file creation keywords.

--- stirrings_still/segments/14/music.py
+++ stirrings_still/segments/14/music.py
@@ -784,9 +784,13 @@ if __name__ == "__main__":
             baca.tags.STAGE_NUMBER,
         ),
         always_make_global_rests=True,
-        global_rests_in_topmost_staff=True,
         error_on_not_yet_pitched=True,
         fermata_measure_empty_overrides=[39],
+        global_rests_in_topmost_staff=True,
+        lilypond_file_keywords=baca.make_lilypond_file_dictionary(
+            include_layout_ly=True,
+            includes=["../../stylesheet.ily"],
+        ),
         score=score,
         stage_markup=stage_markup,
     )
