1443925a5344c297106f3e23fbeda46c310e6fa7
Trevor Bača <trevor.baca@gmail.com>
2022-04-13 15:49:22 +0200

    Rerendered sections.

--- stirrings_still/library.py
+++ stirrings_still/library.py
@@ -3412,7 +3412,7 @@ voice_abbreviations = {
 
 
 def make_empty_score():
-    tag = baca.function_name(inspect.currentframe())
+    tag = baca.tags.function_name(inspect.currentframe())
     global_context = baca.score.make_global_context()
 
     # VIOLIN 1
