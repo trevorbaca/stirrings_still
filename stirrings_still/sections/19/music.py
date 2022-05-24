45014045241267755943beee0454ea1428ab646e
Trevor Bača <trevor.baca@gmail.com>
2022-05-24 17:39:40 +0200

    Bumped LilyPond to 2.23.9.

--- stirrings_still/sections/19/music.py
+++ stirrings_still/sections/19/music.py
@@ -289,7 +289,7 @@ if __name__ == "__main__":
         always_make_global_rests=True,
         error_on_not_yet_pitched=True,
         fermata_measure_empty_overrides=[9, 16, 20],
-        final_segment=True,
+        final_section=True,
         global_rests_in_topmost_staff=True,
         stage_markup=stage_markup,
     )
