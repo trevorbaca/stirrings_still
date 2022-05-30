d60b14b2603fc9e9a939ba636959d1c85495d2f1
Trevor Bača <trevor.baca@gmail.com>
2022-05-30 12:48:41 +0200

    Cleaned up library.

--- stirrings_still/sections/04/music.py
+++ stirrings_still/sections/04/music.py
@@ -47,11 +47,11 @@ voice_names = baca.accumulator.get_voice_names(score)
 
 commands = baca.CommandAccumulator(
     **baca.section_accumulation_defaults(),
-    instruments=library.instruments,
-    margin_markups=library.margin_markups,
-    metronome_marks=library.metronome_marks,
+    instruments=library.instruments(),
+    margin_markups=library.margin_markups(),
+    metronome_marks=library.metronome_marks(),
     time_signatures=library.time_signatures("D"),
-    voice_abbreviations=library.voice_abbreviations,
+    voice_abbreviations=library.voice_abbreviations(),
     voice_names=voice_names,
 )
 
