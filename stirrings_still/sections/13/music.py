ae526028188d8ac9f2c1b7217b502011b8463dc2
Trevor Bača <trevor.baca@gmail.com>
2022-09-25 18:19:04 -0400

    Cleaned up music.py files.

--- stirrings_still/sections/13/music.py
+++ stirrings_still/sections/13/music.py
@@ -525,7 +525,7 @@ def main():
         environment.first_measure_number,
         environment.previous_persist["persistent_indicators"],
     )
-    metadata, persist, timing = baca.build.section(
+    metadata, persist, timing = baca.build.postprocess_score(
         score,
         library.manifests,
         accumulator.time_signatures,
@@ -544,7 +544,7 @@ def main():
         include_layout_ly=True,
         includes=["../stylesheet.ily"],
     )
-    baca.build.persist(
+    baca.build.persist_lilypond_file(
         lilypond_file,
         environment.metadata,
         environment.persist,
