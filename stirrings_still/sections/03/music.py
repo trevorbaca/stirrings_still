a99291f921e9fc7c3f1861924308d50b0c120bf3
Trevor Bača <trevor.baca@gmail.com>
2022-10-05 10:16:09 -0400

    Cleaned up tags.

--- stirrings_still/sections/03/music.py
+++ stirrings_still/sections/03/music.py
@@ -1003,16 +1003,18 @@ def main():
     metadata = baca.section.postprocess_score(
         score,
         **baca.section.section_defaults(),
-        activate=[
-            baca.tags.LOCAL_MEASURE_NUMBER,
-            baca.tags.STAGE_NUMBER,
-        ],
         always_make_global_rests=True,
         environment=environment,
         error_on_not_yet_pitched=True,
         fermata_measure_empty_overrides=[11, 70, 77],
         global_rests_in_topmost_staff=True,
         manifests=library.manifests,
+        tags=baca.tags.Tags(
+            activate=[
+                baca.tags.LOCAL_MEASURE_NUMBER,
+                baca.tags.STAGE_NUMBER,
+            ],
+        ),
     )
     lilypond_file = baca.lilypond.file(
         score,
