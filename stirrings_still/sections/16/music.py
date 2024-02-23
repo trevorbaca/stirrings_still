a67610ce450b166a6d14077e6b2e0f1e55efc6d2
Trevor Bača <trevor.baca@gmail.com>
2024-02-23 09:39:48 -0500

    Blacked code.

--- stirrings_still/sections/16/music.py
+++ stirrings_still/sections/16/music.py
@@ -230,9 +230,7 @@ def v1(cache):
     m = cache[name]
     for item in [(1, 5), (8, 9), (11, 15), (18, 19)]:
         with baca.scope(m.get(item)) as o:
-            baca.glissando(
-                baca.select.rleak(o), "A4", hide_middle_stems=True
-            )
+            baca.glissando(baca.select.rleak(o), "A4", hide_middle_stems=True)
             baca.rspanners.tasto(
                 o,
                 staff_padding=v1_spanner_staff_padding,
@@ -295,9 +293,7 @@ def v2(cache):
     m = cache[name]
     for item in [(1, 4), 6, 8, (10, 14), 16, 18]:
         with baca.scope(m.get(item)) as o:
-            baca.glissando(
-                baca.select.rleak(o), "A3", hide_middle_stems=True
-            )
+            baca.glissando(baca.select.rleak(o), "A3", hide_middle_stems=True)
             baca.rspanners.tasto(
                 o,
                 staff_padding=v2_spanner_staff_padding,
@@ -352,9 +348,7 @@ def va(cache):
     m = cache[name]
     for item in [(1, 7), (11, 17)]:
         with baca.scope(m.get(item)) as o:
-            baca.glissando(
-                baca.select.rleak(o), "A3", hide_middle_stems=True
-            )
+            baca.glissando(baca.select.rleak(o), "A3", hide_middle_stems=True)
             baca.rspanners.tasto(
                 o,
                 staff_padding=va_spanner_staff_padding,
@@ -389,9 +383,7 @@ def vc(cache):
     m = cache[name]
     for item in [(1, 5), (9, 15)]:
         with baca.scope(m.get(item)) as o:
-            baca.glissando(
-                baca.select.rleak(o), "A2", hide_middle_stems=True
-            )
+            baca.glissando(baca.select.rleak(o), "A2", hide_middle_stems=True)
             baca.rspanners.tasto(
                 o,
                 staff_padding=vc_spanner_staff_padding,
